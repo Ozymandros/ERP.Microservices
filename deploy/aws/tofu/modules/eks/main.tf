b186065ef9f3f75a4d2748caa601771f8ad629c5
Andreu Martínez Vidal <andreumv@hotmail.com>
2026-09-24 11:12:40 +0200

    fix tofu authentication_mode

--- deploy/aws/tofu/modules/eks/main.tf
+++ deploy/aws/tofu/modules/eks/main.tf
@@ -41,7 +41,9 @@ resource "aws_eks_cluster" "this" {
   role_arn = aws_iam_role.cluster.arn
   version  = var.kubernetes_version
 
-  authentication_mode = "API_AND_CONFIG_MAP"
+  access_config {
+    authentication_mode = "API_AND_CONFIG_MAP"
+  }
 
   vpc_config {
     subnet_ids              = var.cluster_subnet_ids
