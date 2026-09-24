3ae03dc8e894e168b9989b08916f8a829338ab01
Andreu Martínez Vidal <andreumv@hotmail.com>
2026-09-24 10:50:49 +0200

    tofu formatting: fix whitespaces

--- deploy/aws/tofu/modules/eks/main.tf
+++ deploy/aws/tofu/modules/eks/main.tf
@@ -40,8 +40,9 @@ resource "aws_eks_cluster" "this" {
   name                = "${var.name_prefix}-eks"
   role_arn            = aws_iam_role.cluster.arn
   version             = var.kubernetes_version
+
   authentication_mode = "API_AND_CONFIG_MAP"
-  
+
   vpc_config {
     subnet_ids              = var.cluster_subnet_ids
     endpoint_private_access = true
